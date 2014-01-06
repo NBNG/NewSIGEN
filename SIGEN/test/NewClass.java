b5e03cd444cdd7eae3ad855877faad84d387cbc6
matteuscb <matteuscb@hotmail.com>
2014-01-05 22:02:22 -0200

    2.4.1.18

--- SIGEN/test/NewClass.java
+++ SIGEN/test/NewClass.java
@@ -1,5 +1,5 @@
 
-import br.com.sigen.Modelo.Chapa;
+import br.com.sigen.Modelo.Letra;
 import br.com.sigen.fabrica.ConnectionFactory;
 import java.util.List;
 import org.hibernate.Session;
@@ -8,13 +8,13 @@ public class NewClass {
 
     private Session session = new ConnectionFactory().getSession();
 
-    private String montaQuery(String letra, String quadra) {
-        return "FROM Chapa chapa "
+    private String montaQuery(String quadra) {
+         return "FROM Chapa chapa "
                 + "INNER JOIN chapa.letra as letra"
                 + " INNER JOIN letra.quadra as quadra"
                 + " LEFT JOIN chapa.venda as venda "
                 + "WHERE quadra.quadra ='" + quadra + "' AND "
-                + "letra.letra ='" + letra + "' AND "
+  
                 + "venda.chapa is null"
                 + " ORDER BY quadra.quadra,letra.letra,chapa.chapa";
     }
@@ -24,15 +24,13 @@ public class NewClass {
     }
 
     public void pesquisaVendida() {
-        List<Object[]> list = buscaAvançada(montaQuery("B","A"));
-        Object[] resultado;
-        
-        System.out.println(list.size());
+        List<Object[]> list = buscaAvançada(montaQuery("B"));
+        Object[] resultado;        
 
         for (int i = 0; i < list.size(); i++) {
             resultado = list.get(i);
-            Chapa chapa = (Chapa) resultado[0];
-            System.out.println(chapa.getChapa());
+            Letra letra = (Letra) resultado[1];
+            System.out.println(letra.getLetra());
         }
     }
 
